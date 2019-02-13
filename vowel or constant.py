007715bf06808aa4a67a40144a641d6cf16b2844
smilykavi <43004313+smilykavi@users.noreply.github.com>
2019-02-13 14:16:22 +0530

    Create vowel or constant.py

---
+++ vowel or constant.py
@@ -0,0 +1,7 @@
+x=int(input("enter the value"))
+if(ch=='A',ch=='E',ch=='I',ch=='O',ch=='u'):
+  print("vowel)
+elif(ch=='a',ch=='e',ch=='i',ch=='o',ch=='u'):
+  print("vowel)
+else:
+  print("constant")
