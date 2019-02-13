01184197c396a71185872ff918b4ae035c8f1955
smilykavi <43004313+smilykavi@users.noreply.github.com>
2019-02-13 14:30:30 +0530

    Create largestnum.py

---
+++ largestnum.py
@@ -0,0 +1,9 @@
+x=int(input("enter first number"))
+y=int(input("enter second number"))
+z=int(input("enter third number"))
+if(num1>num2) and (num1>num3):
+  print("large is num one")
+elif(num2>num3) and (num2>num1):
+  print("large is num two")
+else:
+  print("large is num3")
