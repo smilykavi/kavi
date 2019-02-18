1b5b306f830e005c591eaeda2def1f8f24818448
smilykavi <43004313+smilykavi@users.noreply.github.com>
2019-02-18 11:06:27 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+num=int(input("enter the number"))
+temp=num
+rev=0
+while temp!=0:
+ rev=(rev*10)+(temp%10)
+ temp=temp//10
+ if(num==rev):
+    print("number is palindrome")
+    else:
+     print('not palindrome")
