b021b3d4e35a7cfff9d7c6a3d131cf7cd750a8ee
smilykavi <43004313+smilykavi@users.noreply.github.com>
2019-02-18 10:36:30 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,10 @@
+l=int(input("enter the number"))
+u=int(input("enter the number"))
+for num in range(l,u+1):
+sum=0
+while(temp>):
+ digit=temp%10
+ sum+=digit**3
+ temp//=10
+ if(num==sum):
+ print(num)
