51914c00852a96a58b8305d6dce1f6795452c480
smilykavi <43004313+smilykavi@users.noreply.github.com>
2019-02-18 10:49:15 +0530

    Create display prime number.py

---
+++ display prime number.py
@@ -0,0 +1,9 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+for num in range(l,u+1):
+ if(num>i):
+  for i in range(2,num):
+   if (num%1)==0:
+    break
+   else:
+    print(num)
